c95af485aeece1139830d7eff9d087865daa74f4
Laurindo Firmino <Laurindo kandjandja>
2024-08-16 11:10:11 +0100

    feat: addAplication

---
+++ src/Backend/MinhasReceitas.Application/UserCases/User/Register/IRegisterUserUseCase.cs
@@ -0,0 +1,10 @@
+﻿using MinhasReceitas.Communication.Requests;
+using MinhasReceitas.Communication.Responses;
+
+namespace MinhasReceitas.Application.UserCases.User.Register
+{
+    public interface IRegisterUserUseCase
+    {
+        public Task<ResponsesRegisteredUserJson> Execute(RequestRegisterUserJson request);
+    }
+}
---
+++ src/Backend/MinhasReceitas.Application/UserCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using MinhasReceitas.Communication.Requests;
+using MinhasReceitas.Exceptions;
+
+namespace MinhasReceitas.Application.UserCases.User.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+    {
+        public RegisterUserValidator() 
+        {
+            RuleFor(user => user.Nome).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
+            RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessagesException.EMAIL_EMPTY);
+            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
+            RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesException.PASSWORD_EMPTY);
+        }
+    }
+}
