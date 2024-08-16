2e7a83a024b832c9a31f8f24a2d60fe2f3ff7438
Laurindo Firmino <Laurindo kandjandja>
2024-08-16 05:18:41 +0100

    feat: adding exceptons and exceptions messages to register user

---
+++ src/Shared/MinhasReceitas.Exceptions/ExceptionBase/ErrorOnValidationException.cs
@@ -0,0 +1,13 @@
+﻿namespace MinhasReceitas.Exceptions.ExceptionBase
+{
+    public class ErrorOnValidationException : MinhasReceitasException
+    {
+        public IList<string> ErrorMessages { get; set; }
+        public ErrorOnValidationException(IList<string> errorMessages)
+        {
+
+            ErrorMessages = errorMessages;
+
+        }
+    }
+}
