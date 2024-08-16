5ab2dda0bea24baabd8289568a841cb365203109
Laurindo Firmino <Laurindo kandjandja>
2024-08-16 11:09:16 +0100

    feat: addInfra

---
+++ src/Backend/MinhasReceitas.Infra/DataAccess/MinhasReceitasDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhasReceitas.Domain.Entities;
+
+namespace MinhasReceitas.Infra.DataAccess
+{
+    public class MinhasReceitasDbContext : DbContext
+    {
+        public MinhasReceitasDbContext(DbContextOptions options) : base (options) { }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MinhasReceitasDbContext).Assembly);
+        }
+    }
+}
---
+++ src/Backend/MinhasReceitas.Infra/DataAccess/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using MinhasReceitas.Domain.Repositories;
+
+namespace MinhasReceitas.Infra.DataAccess
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MinhasReceitasDbContext _dbContext;
+        public UnitOfWork(MinhasReceitasDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+         
+        public async Task Commit()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+      
+    }
+}
